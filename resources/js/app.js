7899c2f746171300e7406f340c581a035f48ad21
Frize <voisine.kevin2@gmail.com>
2025-02-14 09:32:20 +0100

    [⚙️ build] installation de editorjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,27 @@
 import './bootstrap';
+import EditorJS from '@editorjs/editorjs';
+
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const editor = new EditorJS({
+    holder: 'editorjs',
+    tools: {
+        header: {
+            class: require('@editorjs/header'),
+            inlineToolbar: true,
+        },
+        list: {
+            class: require('@editorjs/list'),
+            inlineToolbar: true,
+        },
+        paragraph: {
+            class: require('@editorjs/paragraph'),
+            inlineToolbar: true,
+        },
+    }
+});
